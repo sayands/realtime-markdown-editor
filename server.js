13e484cea7b177afe6f468eb197c3283c05b141c
sayands <sayandsarkar.1997@gmail.com>
2018-07-30 00:43:23 +0530

    Resolved Tab Issue

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-// server.js
+/* server.js */
+
 var express = require("express");
 var app = express();
 
 // set the view engine to ejs
 app.set("view engine", "ejs");
 
-//public folder to store assets
+// public folder to store assets
 app.use(express.static(__dirname + "/public"));
 
 // routes for app
 app.get("/", function(req, res) {
   res.render("pad");
 });
-
 app.get("/(:id)", function(req, res) {
   res.render("pad");
 });
@@ -29,6 +29,6 @@ var options = {
 // attach the express server to sharejs
 sharejs.server.attach(app, options);
 
-// listen on port 8000
+// listen on port 8000 (for localhost) or the port defined for heroku
 var port = process.env.PORT || 8000;
 app.listen(port);
